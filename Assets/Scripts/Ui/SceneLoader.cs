80d38ea71fdd661f25c7f45e170c1f17d1daf933
Desaster <8desastertigituna873@gmail.com>
2026-07-08 20:03:37 +0200

    Last push

---
+++ Assets/Scripts/Ui/SceneLoader.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextScene : MonoBehaviour
+{
+    public void LoadNextInBuild()
+    {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadTitelScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadOptionScreen()
+    {
+        SceneManager.LoadScene("OptionsMainScreen");
+    }
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+
+}
